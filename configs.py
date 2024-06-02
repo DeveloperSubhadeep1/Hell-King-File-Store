2d9329dae8ce7cc3f0236b9db53424ea984ccab1
DeveloperSubhadeep1 <161279108+DeveloperSubhadeep1@users.noreply.github.com>
2024-06-02 18:59:19 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -42,7 +42,7 @@ class Config(object):
 	BOT_USERNAME = os.environ.get("BOT_USERNAME" , "Hell King Manager")
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", -1001772120203))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "6075512585"))
-	DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://subhadeepsamui79:v3REnnLOfITn2p8t@cluster0.4z3invg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+	DATABASE_URL = os.environ.get("DATABASE_URL",'mongodb+srv://subhadeepsamui79:v3REnnLOfITn2p8t@cluster0.4z3invg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "0")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1002178887238")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "0").split())
